d3abc6e9bccc7c69a5034c5e0897eabe768cdc42
Jeff Killinger <jeffkillinger@gmail.com>
2018-08-22 11:48:23 -0500

    connect comment component

---
+++ components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return (
+      <div className="comment">
+        A comment am I
+      </div>
+    )
+  }
+});
+
+export default Comments;
